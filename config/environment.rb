7d6fda1cfe960c1c49b9809b181aa97ca95e6a7e
omochimogumogu666 <h01itaku@gmail.com>
2023-10-25 07:32:29 +0900

    Add a Static Pages controller

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,3 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-Rails.application.configure do
-    config.hosts << "sample-app-sqlite.onrender.com"
-end
-
